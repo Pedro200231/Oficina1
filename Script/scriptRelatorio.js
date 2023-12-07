19d798d5e78668a762c88437811c604d34313b42
João Pedro Odorizzio de Souza <joao.310502@alunos.utfpr.edu.br>
2023-12-07 08:55:32 -0300

    Fix bug in login functionality

--- Script/scriptRelatorio.js
+++ Script/scriptRelatorio.js
@@ -7,25 +7,49 @@
     function procurar() {
         // Simulação de dados fictícios
         const dadosFicticios = [
-            { nome: 'Aluno 1', nota: 7.5 },
-            { nome: 'Aluno 2', nota: 8.2 },
-            { nome: 'Aluno 3', nota: 6.8 },
-            { nome: 'Aluno 4', nota: 9.0 },
-            { nome: 'Aluno 5', nota: 7.3 },
+            { nome: 'Aluno 1', nota: 7.5, data: '2022-01-01' },
+            { nome: 'Aluno 2', nota: 8.2, data: '2022-01-01' },
+            { nome: 'Aluno 3', nota: 6.8, data: '2022-01-01' },
+            { nome: 'Aluno 1', nota: 8.0, data: '2022-02-01' },
+            { nome: 'Aluno 2', nota: 7.2, data: '2022-02-01' },
+            { nome: 'Aluno 3', nota: 9.0, data: '2022-02-01' },
         ];
+
+        // Agrupar as notas por período e calcular a média
+        const mediasPorPeriodo = {};
+        dadosFicticios.forEach(aluno => {
+            if (!mediasPorPeriodo[aluno.data]) {
+                mediasPorPeriodo[aluno.data] = { total: 0, quantidade: 0 };
+            }
+            mediasPorPeriodo[aluno.data].total += aluno.nota;
+            mediasPorPeriodo[aluno.data].quantidade += 1;
+        });
 
         // Preencher o relatório de notas
         const relatorioNotas = document.getElementById('relatorioNotas');
         dadosFicticios.forEach(aluno => {
-            relatorioNotas.innerHTML += `<p class="nota">${aluno.nome}: ${aluno.nota}</p>`;
+            relatorioNotas.innerHTML += `<p class="nota">${aluno.nome}: ${aluno.nota} (${aluno.data})</p>`;
         });
 
-        // Preencher o gráfico
+        // Preencher o gráfico com as médias por período
         const grafico = document.getElementById('grafico');
-        dadosFicticios.forEach(aluno => {
-            const barra = document.createElement('div');
-            barra.className = 'barra-grafico';
-            barra.style.width = `${aluno.nota * 10}%`;
-            grafico.appendChild(barra);
-        });
+        for (const periodo in mediasPorPeriodo) {
+            if (mediasPorPeriodo.hasOwnProperty(periodo)) {
+                const media = mediasPorPeriodo[periodo].total / mediasPorPeriodo[periodo].quantidade;
+                const barraContainer = document.createElement('div');
+                barraContainer.className = 'barra-container';
+
+                const periodoText = document.createElement('p');
+                periodoText.className = 'periodo-text';
+                periodoText.textContent = `${periodo}`;
+
+                const barra = document.createElement('div');
+                barra.className = 'barra-grafico';
+                barra.style.width = `${media * 10}%`;
+
+                barraContainer.appendChild(periodoText);
+                barraContainer.appendChild(barra);
+                grafico.appendChild(barraContainer);
+            }
+        }
     }
